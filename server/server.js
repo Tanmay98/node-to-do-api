84c7fdc0bd02372af39c3cd11e7b742705d97d8c
Tanmay Baweja <tanmaybaweja1999@gmail.com>
2018-06-23 17:05:27 +0530

    Add GET/todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -27,6 +28,19 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   });
+});
+
+app.get('/todos/:id', (req, res) => {
+    id = req.params.id;
+   if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+   }
+   Todo.findById(id).then((todos) => {
+    if(!todos){
+      return res.status(404).send();
+    }
+    res.send(`todo by ${id} ${todos}`);
+   }).catch((e) => res.status(400).send());
 });
 
 app.listen(3000, () => {
