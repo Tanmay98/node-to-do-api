94f48f550517168c52b323e67f786af74f09405e
Tanmay Baweja <tanmaybaweja1999@gmail.com>
2018-06-24 14:52:17 +0530

    Add tests for PATCH .todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -10,7 +10,9 @@ const todos = [{
   text: 'First test todo'
 }, {
   _id: new ObjectID(),
-  text: 'Second test todo'
+  text: 'Second test todo',
+  completed: true,
+  completedAt: 333
 }];
 
 beforeEach((done) => {
@@ -122,7 +124,7 @@ describe('DELETE /todos/:id', () => {
         }).catch((e) => done(e));
     });
 });
-  it('should remove 404 if todo not found', () => {
+  it('should remove 404 if todo not found', (done) => {
     var hexId = new ObjectID().toHexString();
 
     request(app)
@@ -130,13 +132,47 @@ describe('DELETE /todos/:id', () => {
       .expect(404)
       .end(done);
   });
-  it('should return 404 if id is not valid', () => {
+  it('should return 404 if id is not valid', (done) => {
        request(app)
         .delete('/todos/123abc')
         .expect(404)
         .end(done);
   });
 }); 
+
+describe('PATCH /todos/:id', () => {
+  it('should update the todo', (done) => {
+    var text = 'whateVER wheNEVer';
+    var hexId = todos[0]._id.toHexString();
+    
+    request(app)
+      .patch(`/todos/${hexId}`)
+      .send({text, completed: true})
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(text);
+        expect(res.body.todo.completed).toBe(true);
+        expect(res.body.todo.completedAt).toBeA('number');
+      })
+      .end(done);
+  });
+
+  it('should clear completedAt when todo is not completed', (done) => {
+    var text = 'whateVER wheNEVer !!';
+    var hexId = todos[1]._id.toHexString();
+    
+    request(app)
+      .patch(`/todos/${hexId}`)
+      .send({text, completed: false})
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(text);
+        expect(res.body.todo.completed).toBe(false);
+        expect(res.body.todo.completedAt).toNotExist();
+      })
+      .end(done);
+  });
+});
 
 
 
